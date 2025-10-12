fab6bf4d92b8145dc2f97864b255c14ad5ad9a2d
BlueSkyDream7 <133848148+BlueSkyDream7@users.noreply.github.com>
2025-10-12 17:14:17 +0800

    Update dual_lstm.py

--- models/dual_lstm.py
+++ models/dual_lstm.py
@@ -4,28 +4,28 @@ from torch.nn import functional as f
 
 class DualLSTM(nn.Module):
     def __init__(self, input_size, output_size, hidden_size, dr=0.0):
-        super(DualLSTM, self).__init__()                      # 继承父类初始化函数
-        # self.bn1 = nn.BatchNorm1d(input_size, affine=False)     # 归一化层（input_size个输入和输出）
-        self.linearIn = nn.Linear(input_size, hidden_size)      # 线性输入层（input_size个输入 hidden_size个输出）
+        super(DualLSTM, self).__init__()                        
+        # self.bn1 = nn.BatchNorm1d(input_size, affine=False)    # Batch Normalization                   
+        self.linearIn = nn.Linear(input_size, hidden_size)       # Linear Fully Connected Input Layer
         self.lstm1 = nn.LSTM(
             hidden_size,
             hidden_size,
             1,
             dropout=dr,
-        )                                                       # LSTM层（hidden_size个输入 hidden_size个输出 无dropout）
+        )                                                        # LSTM Layer 1
         self.lstm2 = nn.LSTM(
             hidden_size,
             hidden_size,
             1,
             dropout=dr,
-        )
-        self.linearOut = nn.Linear(hidden_size, output_size)    # 线性输出层（hidden_size个输入 output_size个输出）
+        )                                                        # LSTM Layer 2
+        self.linearOut = nn.Linear(hidden_size, output_size)     # Linear Fully Connected Output Layer
 
-    def forward(self, x):                                       # 定义神经网络的前向传播函数forward，x即为输入
+    def forward(self, x):                                        # Forward propagation function
         x0 = f.leaky_relu(self.linearIn(x))
         out_lstm1, (hn, cn) = self.lstm1(x0)
         out_lstm2, (hn, cn) = self.lstm2(out_lstm1)
-        return self.linearOut(out_lstm2)                         # 线性输入 -> ReLU -> LSTM1 -> LSTM2 -> 线性输出
+        return self.linearOut(out_lstm2)                         # LinearIn -> ReLU -> LSTM1 -> LSTM2 -> LinearOut
 
 
 class DualLSTMForecast(DualLSTM):
@@ -35,9 +35,10 @@ class DualLSTMForecast(DualLSTM):
         self.output_size = output_size
 
     def forward(self, x):
-        # 调用父类的forward方法获取完整的输出
+        # Call the forward method of the parent class to obtain the complete output
         full_output = super(DualLSTMForecast, self).forward(x)
 
-        # 重塑输出以匹配目标形状
+        # Reshape output
         forecast_output = full_output[-self.forecast_length:, :, :]
         return forecast_output
+
